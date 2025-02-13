1461f8cac72475fcc37afd16473208e30040f110
Martinelii <Kevenmartineli@outlook.com>
2025-02-13 14:13:18 -0300

    fix: responsividade NavBar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -4,22 +4,22 @@ function NavBar() {
     return (
         <div className='flex flex-row items-center justify-between h-16 shadow-md px-4'>
             <span className='text-2xl font-bold text-gray-900'>KM</span>
-            <div className='flex space-x-4'>
+            <div className='flex space-x-1 md:space-x-4'>
                 <a href="" 
-                className='text-gray-700 px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
+                className='text-gray-700 px-1 md:px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
                     About
                 </a>
                 <a href=""
-                className='text-gray-700 px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
+                className='text-gray-700 px-1 md:px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
                     Work</a>
                 <a href=""
-                className='text-gray-700 px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
+                className='text-gray-700 px-1 md:px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
                     Education</a>
                 <a href=""
-                className='text-gray-700 px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
+                className='text-gray-700 px-1 md:px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
                     Projects</a>
                 <a href=""
-                className='text-gray-700 px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
+                className='text-gray-700 px-1 md:px-3 py-2 rounded-sm font-medium hover:text-gray-900 hover:bg-gray-200'>
                     Contact</a>
             </div>
         </div>
